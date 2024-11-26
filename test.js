cc81099c55dc3a461c9555a366b7245eb71eb4f7
cecil-7576 <iceguvera@gmail.com>
2024-11-26 13:27:18 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,41 @@
+document.write ("Hello World");
+            document.write (", I Love You All");
+            document.write ("come on lads")
+
+            var day;  // Must alway start with a VAR
+            var assignValue = 0725;
+            var gameOver = true;
+            var years = 40;
+            var someText = "years old";
+            alert(years + someText);
+
+function saySomething () {
+// All Statements goes inside the curly braces 
+alert ("This is a function");
+            }
+
+saySomething ();
+
+function addSomething (num,str) {
+    // All Statements goes inside the curly braces 
+    var add = num + str;
+    alert (add);
+                }
+    
+    addSomething (455," Programes to write");
+
+    function minusSomething (num,str) {
+        var minus = num + str;
+        alert (minus); 
+                        }
+    minusSomething (0725," Pages to add")
+
+    // Arithmetic and Assignment Operators 
+
+    var number1 = 7;
+    var number2 = 4;
+
+    var number1 = number1 + number2;
+
+    document.write (number1);
+
