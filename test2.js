1c3c77757b08c63f64f584fed9256f9103baedb7
cecil-7576 <iceguvera@gmail.com>
2024-12-03 22:05:54 +0100

    Update test2.js

--- test2.js
+++ test2.js
@@ -2,3 +2,21 @@
 alert("Hello World");
 //this is second alert How to write a comment 
 alert("How are you");
+
+let myName = "Byran";
+let myAge = 16; //Replace with your age
+
+if (myAge >= 18) {
+    console.log(myName + ", you are eligible to vote.");
+} else {
+    console.log(myName + ", you are not eligible to vote.");
+}
+
+let balance = 10000;
+let withdrawal = 5000;
+
+if (withdrawal <= balance) {
+    console.log('Transaction successful');
+} else {
+    console.log('insuficient funds !!!');
+}   
