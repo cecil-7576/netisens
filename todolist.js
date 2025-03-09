7f1e6e2c3eee0618525f0822059be7c08e2ed570
cecil-7576 <iceguvera@gmail.com>
2025-03-09 19:39:26 +0100

    Update todolist.js

--- todolist.js
+++ todolist.js
@@ -1,5 +1,5 @@
 // Dom selectors 
-const inputVAlue = document.getElementById('inputDisplay');
+const inputValue = document.getElementById('inputDisplay');
 const addNewTask = document.querySelector('.btn');
 const clearAllButton = document.querySelector('.ctn');
 const filterSelect = document.querySelector('#filter-todos');
@@ -8,234 +8,176 @@ const myTodoList = document.querySelector('#taskList');
 // variable to hold the current task text
 let task;
 
+// load existing todos from localStorage, if available, or initialize an empty array
+let todos = JSON.parse(localStorage.getItem('todos')) || [];
+console.log('todos: ', todos);
+updateScreen();
+
 // variable to hold the structure of the new task as an object
 let todoObject;
 
-// load existing todos from localStorage, if available, or initialize an empty array
-let todos = JSON.parse(localStorage.getItem('todos')) || [];
-//console.log('todos: ', todos);
-updateScreen(); // display the tasks on the screen 
+//function to create a new task from the user's input 
+function createTodo(e){
+    task = inputValue.value.trim();
 
-// function to create a new task from the user's input 
-function createTodo (e) {
-    task = inputVAlue.value.trim(); // get the value of the task input, trimmed of whitespace
-    console.log(task)
-    
-    if (task === "") {
+    if(task === ""){
         // exit early if the task input is empty
         alert('Please enter a valid task!');
         return;
     }
 
-    // create a new task object with the task text and mark it as not completed
+    //create a new task object with the task text and mark it as not completed
     todoObject = {
-    task: task,
-    completed: false
+        task: task,
+        completed: false
     };
 
-    // check if the "Enetr" key (key code 13) was pressed to add the task 
-    if (e.keyCode=== 13) {
-        addToTodosArray(); 
+    //check if the "Enter" key (key code 13) was pressed to add the task
+    if (e.keyCode === 13){
+        addTodosArray(); //add the task to the todo array
     }
-    updateScreen()
-}
+};
 
-// add an event listener to the input field to listen for "Enter" key presses
-inputVAlue.addEventListener('keyup', e=>createTodo (e) );
-
-
-// function to add the new task to the object to the todos array
-function addToTodosArray () {
-    todos.push(todoObject);
-    localStorage.setItem('todos', JSON.stringify(todos));
-    updateScreen();
-
-}
-
-
- // function to add the new task to the todo array
- function addTodoToArray () {
-    if (todoObject.task){
+//function to add the new task to the todos array
+function addTodosArray(){
+    if(todoObject.task){
         todos.push(todoObject)
         localStorage.setItem('todos', JSON.stringify(todos));
+        updateScreen();
     }else {
-        alert('No empty array')
+        alert('No empty task!')
     }
-    addToTodosArray()
-    updateScreen()
 }
 
-// add an event listener to the "add" button to add a new task when clicked 
-addNewTask.addEventListener('click', function (){
-    if (inputVAlue.value.trim() === "") {
-        alert('Please enter a valid task!');
-    } else {
-        addToTodosArray();
+// add an event listeenr to the input field to listen for "Enter" key presses
+inputValue.addEventListener('keyup', function (e){
+    createTodo(e);
+});
+
+// add an event listener to the "Add" button ti add a new task when clicked 
+addNewTask.addEventListener('click', function(){
+    if(task === ''){
+        alert('Please enter a valid task!')
+    }else {
+        addTodosArray()
     }
 });
 
+//function to add the new task to the todos array
+function addTodosArray(){
+    todos.push(todoObject);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    updateScreen()
+}
+
+console.log(todos);
 
 
-// function to filter todos based on the selected option (all, pending completd)
+//function to filter todos based on the selected options (all, pending, completed)
 
 let filter;
+
 let filteredTodos;
-let todo;
 
-function applyFilter (){
-    filter = filterSelect.value; //get the selected filter value
-    console.log({filter})
-    filteredTodos = []; // initialize an empty array to hold filter todos
-    
-    // use a for loop to iterate over the todso array
-    for (let i = 0; i< todos.length; i++) {
-        todo = todos[i]; // access the current todo
+let todo
 
-        //check the filter value and push the appropriate todos into the filteredTodos array
-        if (filter === 'pending' && !todo.Completed) {
-            filteredTodos.push(todo); //add to the filteredTodos if its pending
-        }else if (filter === 'completed' && !todo.Completed) {filteredTodos.push(todo); //add to filteredTodos if its completed 
+function setFilter(){
+    filter = filterSelect.value 
+    console.log({filter});
 
+    filteredTodos = [];
+
+    todos.forEach(function(text){
+        todo = text;
+
+        if (filter === 'pending' && !todo.completed){
+            filteredTodos.push(todo);
+        } else if (filter === 'completed' && !todo.completed){
+            filteredTodos.push(todo);
         }
-    }
+        updateScreen
+    });
 
-    console.log({filteredTodos})
-    // update the screen with the filtered todos or all todos if no filter is applied 
-    if (filter === 'all') {
-        updateScreen(todos);// show all tasks
+    console.log({filteredTodos});
+
+    if (filter === 'all'){
+        updateScreen(todos); // show all task
     } else {
-        updateScreen(filteredTodos); //pass filtered todos to update the screen 
+        updateScreen(filteredTodos); // pass filter todos to update the screen
     }
 }
 
-filterSelect.addEventListener('change', function (e){
-    applyFilter();
-    updateScreen()
-    addToTodosArray()
-})
 
-// function to delete a specific task from the todos array
-function deleteTodo(index) {
-    todos.splice(index, 1);
-    localStorage.setItem('todos', JSON.stringify(todos));
-    applyFilter();
-    updateScreen()
-}
-
-// function to clear all tasks
-// function clearAllTodos(){
-//     todos = []
-//     localStorage.setItem('todos', JSON.stringify(todos));
-//     clearAllTodos = clearAllButton
-//     clearAllButton.offsetHeight >= 300?
-//     clearAllButton.classList.add('overflow') : clearAllButton.classList.remove('overflow');
-//     updateScreen()
-// }
-
-clearAllButton.addEventListener('click', function(){
+function clearAll(){
     todos = [];
     localStorage.setItem('todos', JSON.stringify(todos));
-    
-    updateScreen(); 
-});
-
-// add a function to toggle the completion state of a task
-function toggleTaskCompletion(index) {
-    todos[index].completed = !todos[index].completed
-    localStorage.setItem('todos', JSON.stringify(todos));
     updateScreen();
-    applyFilter()
 }
 
+clearAllButton.addEventListener('click', clearAll())
 
-// function to edit an existing task 
-function editTodo(index) {
-    //const taskToEdit = prompt(`Edit your task`, todos[index].task);
-    todos[index].task = prompt(`Edit your task`, todos[index].task);
+function toggleTaskCompletion(index){
+    todos[index].complete = !todos[index].complete;
     localStorage.setItem('todos', JSON.stringify(todos));
-    applyFilter();
     updateScreen()
 }
 
-//function to update the UI by giving the current list of todos
-function updateScreen() {
-    // clear the myTodoList to remove any existing tasks in the UI before rendering the new list
-    myTodoList.innerHTML = '';
+filterSelect.addEventListener('change', setFilter);
+
+function editTodos(index){
+    todos[index].task = prompt('Edit your task here!', todos[index].task);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    setFilter()
 }
-// reset the input field value to an empty string after adding/updating tasks
-if(todos.length < 1) {
-    myTodoList.innerHTML = '<h3>No tasks found</h3>';
-    // exit the function early if no tasks are available 
-}  
 
-// loop through the todos array to dynamically create and display each task using template
-for (let index = 0; index < todos.length; index++) {
-    //add a new list item for each todo task and append it directly to the myTodoList's HTML
-    myTodoList.innerHTML += `
-    <li class="task">
-                <div class="content">
-                    <!-- Checkbox for marking the task as completed, binding the checked state to the task's isCompleted property -->
-                    <input type="checkbox" onchange='toggleTaskCompletion(${index})' id="check-btn-${index}"${todos[index].completed ? 'checked' : ''}>
+function deleteTodos(index){
+   todos.splice(index, 1);
+   localStorage.setItem('todos', JSON.stringify(todos));
+   setFilter();
+}
 
-                    <!-- Display the task with edit and delete buttons -->
-                    <div class="spans">
+function updateScreen(){
+    myTodoList.innerHTML = "";
 
+    inputValue.value = '';
+
+    if (todos.length < 1
+
+    ){
+        myTodoList.innerHTML = '<h3>No tasks found</h3>'
+        return;
+    }
+
+    for (let index = 0; index < todos.length; index++){
+
+        myTodoList.innerHTML +=`
+        <li class="task">
+            <div class="content">
+                <!-- Checkbox for marking the task as completed, binding the checked state to the task's isCompleted property -->
+                <input type="checkbox" onchange="toggleTaskCompletion(${index})" id="check-btn-${index}" ${todos[index].complete? 'checked' : ''}>
+                
+                <!-- Display the task with edit and delete buttons -->
+                <div class="spans">
                     <!-- Label shows the text of the task -->
                     <label class="text" for="check-btn-${index}">${todos[index].task}</label>
-
+                    
                     <!-- Container for edit and delete buttons -->
                     <span class="settings">
+                        <!-- Edit button to allow the user to edit the task -->
+                        <button onclick="editTodos(${index})">
+                            <i class="fa fa-pencil" aria-hidden="true"></i>Edit
+                        </button>
 
-                    <!-- Edit button to allow the user to edit the task -->
-                        <button onclick="editTodo(${index})"><i class="bi bi-pen-fill"></i>Edit</button>
-
-                    <!-- Delete button to allow the user to delete the task -->
-                        <button onclick="deleteTodo(${index})"><i class="bi bi-trash"></i>Delete</button>
-                        </span>
-                    </div>
+                        <!-- Delete button to allow the user to delete the task -->
+                        <button onclick="deleteTodos(${index})">
+                            <i class="fa fa-trash"></i>Delete
+                        </button>
+                    </span>
                 </div>
-            </li>`
+            </div>                
+        </li>`;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
